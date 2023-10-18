24a1d04ffb49633c8e6a62c049ff29a2f6077f12
Heng Kang Lin <kanglin0619@gmail.com>
2023-10-18 17:46:01 +0800

    Update App.js

--- project/src/App.js
+++ project/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactabc
+          Learn React
         </a>
       </header>
     </div>
