c045c1581b0cd091a640dd458f7275d77df449d7
HKLin0619 <kanglin0619@gmail.com>
2023-11-02 10:43:11 +0800

    Login Backup setup

--- project/server/server.js
+++ project/server/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+// app.get("/api", (req, res) => {
+//     res.json({"users": ["userOne", "userTwo", "userThree"]})
+// })
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/login', (req, res) => {
+
+    const { username, password } = req.body;
+
 })
 
 app.listen(5000, () => { console.log("Server started on port 5000") })
--- project/src/Components/Content/LoginSignUpContent.jsx
+++ project/src/Components/Content/LoginSignUpContent.jsx
@@ -1,26 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./LoginSignUpContentStyle.css";
 
 function LoginSignUpContent() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+
+    }
+
   return (
     <div className="main">
 
         <div className="sub-main">
 
             <div className='title'>
-                <h1>ERC 20 Tokem Exchange</h1>
+                <h1>ERC 20 Token Exchange</h1>
                 <div className='underline'></div>
             </div>
 
             <div className='inputs'>
                 <div className='input'>
                     <i className="fa-solid fa-user"/>
-                    <input type="text" placeholder="Username" className="name"/>
+                    <input
+                        type="text" 
+                        placeholder="Username" 
+                        className="name" 
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value)}/>
                 </div>
 
                 <div className='input'>
                     <i className="fa-solid fa-lock"/>
-                    <input type="password" placeholder="Password" className="name"/>
+                    <input 
+                        type="password" 
+                        placeholder="Password" 
+                        className="name"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}/>
                 </div>
             </div>
 
@@ -29,7 +59,7 @@ function LoginSignUpContent() {
                 <a href="/">Forgot password ?</a>
             </div>
 
-            <button className='btn'>Login</button>
+            <button className='btn' onClick={handleLogin}>Login</button>
 
             <div className="signin-signup">
                 <p>Don't have an account ? <a href='/' className='signup-link'>Sign Up</a></p>
