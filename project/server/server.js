e1d3cee6eb3957f628329cf8dbaa27cea30ebc1e
kevintan01 <115685030+kevintan01@users.noreply.github.com>
2023-11-02 23:41:08 +0800

    Update server.js

--- project/server/server.js
+++ project/server/server.js
@@ -17,9 +17,9 @@ app.post('/login', (req, res) => {
     database.query('SELECT * FROM "User" WHERE "userName" = $1 AND "password" = $2', [username, password]).then(result => {
         //const dbPassword = result.rows[0].password;
         if (result.rows.length === 1) {
-            res.send('Login Successful !');
+            alert('Login Successful !');
         } else {
-            res.send('Login failed, please check username and password !');
+            alert('Login failed, please check username and password !');
         }
     }).catch(error => {
         console.error('Database Error: ', error);
