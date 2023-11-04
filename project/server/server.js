51aa5b2757750cf742dee39b7747f7d22c5f0d76
Heng Kang Lin <kanglin0619@gmail.com>
2023-11-05 01:40:33 +0800

    Update Login Page
    fail login notification

--- project/server/server.js
+++ project/server/server.js
@@ -10,20 +10,56 @@ app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    database.query('SELECT * FROM "User" WHERE "userName" = $1 AND "password" = $2', [username, password]).then(result => {
+    // database.query('SELECT * FROM "User" WHERE "userName" = $1 AND "password" = $2', [username, password]).then(result => {
+    //     if (result.rows.length === 1) {
+    //         const userType = result.rows[0].userType;
+    //         res.json({ success: true, userType });
+    //         //res.send('Login Successful !');
+    //     } else {
+    //         if (result.rows.length === 0) {
+    //             res.send('u');
+    //         } else {
+    //             res.send('p');
+    //         }
+    //         //res.json({ success: false });
+    //         //res.send('Login failed, please check username and password !');
+    //     }
+    // }).catch(error => {
+    //     console.error('Database Error: ', error);
+    //     res.json({ success: false });
+    //     //res.send('Login failed and a database error occurred !')
+    // });
+
+
+    database.query('SELECT * FROM "User" WHERE "userName" = $1', [username]).then(result => {
+
         if (result.rows.length === 1) {
-            const userType = result.rows[0].userType;
-            res.json({ success: true, userType });
-            //res.send('Login Successful !');
+
+            const dbPassword = result.rows[0].password;
+
+            if (dbPassword === password) {
+
+                const userType = result.rows[0].userType
+                res.json({ success: true, userType });
+
+            } else {
+
+                res.json({ success: false, errorType: 'password' });
+
+            }
+
         } else {
-            res.json({ success: false });
-            //res.send('Login failed, please check username and password !');
+
+            res.json({ success: false, errorType: 'username' });
+
         }
+
     }).catch(error => {
+
         console.error('Database Error: ', error);
         res.json({ success: false });
-        //res.send('Login failed and a database error occurred !')
-    });
+
+    })
 
 });
 
--- project/src/Components/Content/LoginContent.js
+++ project/src/Components/Content/LoginContent.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate  } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "./LoginContentStyle.css";
 
 function LoginSignUpContent() {
@@ -22,15 +24,84 @@ function LoginSignUpContent() {
 
         console.log(data);
 
-        if (data.success) {
-            const userType = data.userType;
-            navigate(`/${userType}dashboard`);
+        if (!username && !password) {
+            toast.error('Enter username and password !', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+        } else {
+
+            if (data.success) {
+                const userType = data.userType;
+                navigate(`/${userType}dashboard`);
+            } else {
+                if (data.errorType === 'username') {
+                    toast.error('Your username is incorrect !', {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
+                    });
+                    setUsername('');
+                    setPassword('');
+                } else if (data.errorType === 'password') {
+                    toast.error('Your password is incorrect !', {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
+                    });
+                    setPassword('');
+                } else {
+                    toast.error('Login failed !', {
+                        position: "top-center",
+                        autoClose: 5000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "colored",
+                    });
+                    setUsername('');
+                    setPassword('');
+                }
+            }
+
         }
 
     }
 
   return (
+    
     <div className="main">
+
+        <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+        />
 
         <div className="sub-main">
 
