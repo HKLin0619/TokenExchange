f1880a423c1e99565e708f562fe37e1355800b0f
kevintan01 <115685030+kevintan01@users.noreply.github.com>
2023-11-06 16:21:52 +0800

    update web3.js can be store tokens into remix.

--- project/server/routes/web3.js
+++ project/server/routes/web3.js
@@ -9,36 +9,35 @@ router.use((req, res, next) => {
 });
 
 router.post('/mintToken', async (req, res) => {
-  const { tokenSymbol, noOfToken } = req.body;
-  if (tokenSymbol === undefined || noOfToken === undefined || isNaN(noOfToken)) {
-    return res.status(400).send({ status: '400', message: 'Invalid input.' });
-  }
+    const { tokenSymbol, noOfToken } = req.body;
+    if (tokenSymbol === undefined || noOfToken === undefined || isNaN(noOfToken)) {
+        return res.status(400).send({ status: '400', message: 'Invalid input.' });
+    }
 
-  const tokenName = await query('SELECT "Name" FROM "Token" where "Symbol" = $1;', [tokenSymbol]).then((res) => res.rows[0]);
+    const tokenName = await query('SELECT "Name" FROM "Token" where "Symbol" = $1;', [tokenSymbol]).then((res) => res.rows[0]);
 
-  if (tokenName === undefined) {
-    return res.status(400).send({ status: '400', message: 'Invalid token.' });
-  }
+    if (tokenName === undefined) {
+        return res.status(400).send({ status: '400', message: 'Invalid token.' });
+    }
 
-  // Declare contractAddress at an outer scope
-  let contractAddress;
+    // Declare contractAddress at an outer scope
+    let contractAddress;
 
-  const result = await tokenContract.deploy({
-    data: bytecode,
-    arguments: [tokenSymbol, tokenName.Name, noOfToken],
-  })
-    .send({
-      from: '0x71Ea4935dAA0F90124f870DDd788AFD17c65F6F3',
-      gas: 6721975,
-      gasPrice: 20000000000,
+    const result = await tokenContract.deploy({
+        data: bytecode,
+        arguments: [tokenSymbol, tokenName.Name, noOfToken], // Use noOfToken as totalSupply
     })
-    .on('receipt', (receipt) => {
-      contractAddress = receipt.contractAddress; // Store the contract address here
-      console.log('Contract deployed at address: ' + contractAddress);
-      // You can now use 'contractAddress' for further interactions
-    });
+        .send({
+            from: '0x71Ea4935dAA0F90124f870DDd788AFD17c65F6F3',
+            gas: 6721975,
+            gasPrice: 20000000000,
+        })
+        .on('receipt', (receipt) => {
+            contractAddress = receipt.contractAddress; // Store the contract address here
+            console.log('Contract deployed at address: ' + contractAddress);
+            // You can now use 'contractAddress' for further interactions
+        });
 
-    
 
   return res.status(200).send({
     
