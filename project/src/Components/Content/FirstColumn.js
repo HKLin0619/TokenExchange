ec21acf587973080c70338179577ea09b20980f8
HKLin0619 <kanglin0619@gmail.com>
2023-11-07 15:54:41 +0800

    Update

--- project/src/Components/Content/FirstColumn.js
+++ project/src/Components/Content/FirstColumn.js
@@ -12,10 +12,21 @@ function FirstColumn(props) {
                 <div className="f-underline"></div>
             </div>
 
-            <div className="f-contents">
+            <div className="f-content">
                 
-                <div className="f-content">
-                    <p>User Type : Admin</p>
+                <div className="f-item  ">
+                    <span className="label">Name:</span>
+                    <span className="value">Heng Kang Lin</span>
+                </div>
+
+                <div className="f-item  ">
+                    <span className="label">Email:</span>
+                    <span className="value">admin@gmail.com</span>
+                </div>
+
+                <div className="f-item  ">
+                    <span className="label">User Type:</span>
+                    <span className="value">Admin</span>
                 </div>
 
             </div>
