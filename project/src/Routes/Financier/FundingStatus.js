1d151ec61e6b894b92e3ff640c66690162c78f42
Heng Kang Lin <kanglin0619@gmail.com>
2024-03-10 01:24:30 +0800

    small update

--- project/src/Routes/Financier/FundingStatus.js
+++ project/src/Routes/Financier/FundingStatus.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from "../../Components/Navbar/Financier/Navbar";
 import Content from '../../Components/Content/Financier/FundingStatus'
 import { useLocation } from 'react-router-dom';
 
@@ -6,6 +7,7 @@ function FundingStatus() {
   const location = useLocation();
   return (
     <div className='main-content'>
+      <Navbar currentPage={location.pathname} />
         <Content/>
     </div>
   )
