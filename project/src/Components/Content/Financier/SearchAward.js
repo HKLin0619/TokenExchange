ba6848bed5c94f200cc70885fe571b51a9dd3a13
Nghongzhan <hongzhanng@yahoo.com.my>
2024-03-16 16:07:57 +0800

    find out solution to open the ipfs document in new page

--- project/src/Components/Content/Financier/SearchAward.js
+++ project/src/Components/Content/Financier/SearchAward.js
@@ -1,82 +1,99 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function SearchAward() {
-    const [awardID, setAwardID] = useState("");
-    const navigate = useNavigate();
-    const location = useLocation();
+  const [awardID, setAwardID] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [documenthash, setDocumentHash] = useState("");
 
-    const handleBack = () => {
-        navigate("/financierdashboard");
-    };
+  const handleBack = () => {
+    navigate("/financierdashboard");
+  };
 
-    const handleSubmit = async () => {
-        try {
-            const response = await fetch("/searchaward", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ awardID }),
-            });
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("/searchaward", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ awardID }),
+      });
 
-            console.log(response)
-            if (!response.ok) {
-                throw new Error("Request failed with status: " + response.status);
-            }
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Request failed with status: " + response.status);
+      }
 
-            
+      const data = await response.json();
+      console.log(data);
+      //console.log("document" + data.document);
 
-            const data = await response.json();
-            console.log(data);
+      const result = await fetch("/searchawardid?awardid=" + awardID, {
+        method: "GET",
+      });
+      const output = await result.json();
+      console.log("checking:");
+      console.log(output);
 
-            if (data.status === 200) {
-                console.log("Success");
-                // Navigate to "/searchawardid" route passing the awardID as a query parameter
-                navigate(`/financierdashboard/searchawardid?awardID=${awardID}`);
-            } else {
-                console.log("No matching awardID found.");
-            }
-        } catch (error) {
-            console.error(error);
-            // Handle errors here, such as displaying an error message to the user
-        }
-    };
+      const award = output.data[0].award_doc_hash;
+      //setDocumentHash(award.award_doc_hash || "");
+      console.log("checking:" + award);
 
-    // Extract awardID from query parameters when component mounts
-    useEffect(() => {
-        const searchParams = new URLSearchParams(location.search);
-        const paramAwardID = searchParams.get("awardID");
-        if (paramAwardID) {
-            setAwardID(paramAwardID);
-            // Now you can perform further actions based on the awardID
-            // For example, fetch additional data based on the awardID
-        }
-    }, [location.search]);
+      if (data.status === 200) {
+        console.log("Success");
+        // Navigate to "/searchawardid" route passing the awardID as a query parameter
+        window.open("https://ipfs.io/ipfs/" + award);
+        navigate(`/financierdashboard/searchawardid?awardID=${awardID}`);
+      } else {
+        console.log("No matching awardID found.");
+      }
+    } catch (error) {
+      console.error(error);
+      // Handle errors here, such as displaying an error message to the user
+    }
+  };
 
-    return (
-        <div className="tm-main">
-            <div className="tm-sub-main">
-                <div className='tm-title'>
-                    <h1>Search Award</h1>
-                    <div className='tm-underline'></div>
-                </div>
-                <div className='tm-inputs'>
-                    <div className='tm-input'>
-                        <i className="fa-solid fa-coins"/>
-                        <input
-                            type="text"
-                            placeholder="AwardID"
-                            className="tm-name"
-                            value={awardID}
-                            onChange={(e) => setAwardID(e.target.value)}/>
-                    </div>
-                </div>
-                <button className='tm-btn' onClick={handleSubmit}>Search</button>
-                <button className="tm-btn" onClick={handleBack}>Back</button>
-            </div>
+  // Extract awardID from query parameters when component mounts
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const paramAwardID = searchParams.get("awardID");
+    if (paramAwardID) {
+      setAwardID(paramAwardID);
+      // Now you can perform further actions based on the awardID
+      // For example, fetch additional data based on the awardID
+    }
+  }, [location.search]);
+
+  return (
+    <div className="tm-main">
+      <div className="tm-sub-main">
+        <div className="tm-title">
+          <h1>Search Award</h1>
+          <div className="tm-underline"></div>
         </div>
-    );
+        <div className="tm-inputs">
+          <div className="tm-input">
+            <i className="fa-solid fa-coins" />
+            <input
+              type="text"
+              placeholder="AwardID"
+              className="tm-name"
+              value={awardID}
+              onChange={(e) => setAwardID(e.target.value)}
+            />
+          </div>
+        </div>
+        <button className="tm-btn" onClick={handleSubmit}>
+          Search
+        </button>
+        <button className="tm-btn" onClick={handleBack}>
+          Back
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default SearchAward;
--- project/src/Components/Content/Financier/SearchAwardID.js
+++ project/src/Components/Content/Financier/SearchAwardID.js
@@ -1,107 +1,107 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./SearchAwardIDStyle.css";
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 
 function SearchAwardID() {
-    const [userid, setUserID] = useState('');
-    const [awardid, setAwardID] = useState('');
-    const [supplierid, setSupplierID] = useState('');
-    const [awardamount, setAwardAmount] = useState('');
-    const [document, setDocument] = useState('');
-    const [documenthash, setDocumentHash] = useState('');
-    const [fundstatus, setFundStatus] = useState('');
-    const navigate = useNavigate();
-    
-    const location = useLocation();
+  const [userid, setUserID] = useState("");
+  const [awardid, setAwardID] = useState("");
+  const [supplierid, setSupplierID] = useState("");
+  const [awardamount, setAwardAmount] = useState("");
+  const [document, setDocument] = useState("");
+  const [documenthash, setDocumentHash] = useState("");
+  const [fundstatus, setFundStatus] = useState("");
+  const navigate = useNavigate();
 
-    const handleBack = async () => {
-        navigate("/financierdashboard/searchaward");
-      };
-    
-    useEffect(() => {
-        const searchParams = new URLSearchParams(location.search);
-        const paramAwardID = searchParams.get("awardID");
-        fetchAwardID(paramAwardID);
-    }, [location.search]);
+  const location = useLocation();
 
-   const fetchAwardID = async (paramAwardID) => {
+  const handleBack = async () => {
+    navigate("/financierdashboard/searchaward");
+  };
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const paramAwardID = searchParams.get("awardID");
+    fetchAwardID(paramAwardID);
+  }, [location.search]);
+
+  const fetchAwardID = async (paramAwardID) => {
     try {
-        const response = await fetch("/searchawardid?awardid=" + paramAwardID, {
-            method: "GET",
-        });
-        const result = await response.json();
-        console.log(result);
+      const response = await fetch("/searchawardid?awardid=" + paramAwardID, {
+        method: "GET",
+      });
+      const result = await response.json();
+      console.log(result);
 
-        if (response.status === 200) {
-            const awardData = result.data[0];
-            setUserID(awardData.buyerid || ''); 
-            setAwardID(awardData.awardid || '');
-            setSupplierID(awardData.supplierid || '');
-            setAwardAmount(awardData.awardamount || '');
-            setDocument(awardData.document || '');
-            setDocumentHash(awardData.award_doc_hash || '');
-            setFundStatus(awardData.funded_ind || '');
-        } else {
-            console.error("Failed to fetch award ID:", result.message);
-        }
+      if (response.status === 200) {
+        const awardData = result.data[0];
+        setUserID(awardData.buyerid || "");
+        setAwardID(awardData.awardid || "");
+        setSupplierID(awardData.supplierid || "");
+        setAwardAmount(awardData.awardamount || "");
+        setDocument(awardData.document || "");
+        setDocumentHash(awardData.award_doc_hash || "");
+        setFundStatus(awardData.funded_ind || "");
+      } else {
+        console.error("Failed to fetch award ID:", result.message);
+      }
     } catch (error) {
-        console.error("Error fetching award ID:", error);
+      console.error("Error fetching award ID:", error);
     }
-};
-    return (
-        <div className="tm-main">
-            <div className="tm-sub-main">
-                <div className='tm-title'>
-                    <h1>Award ID Result</h1>
-                    <div className='tm-underline'></div>
-                </div>
+  };
+  return (
+    <div className="tm-main">
+      <div className="tm-sub-main">
+        <div className="tm-title">
+          <h1>Award ID Result</h1>
+          <div className="tm-underline"></div>
+        </div>
 
-                <div className='tm-inputs'>
-                    <div className='tm-input'>
-                        <i className="fa-solid fa-user"/>
-                        <input
-                            type="text" 
-                            // placeholder="UserID" 
-                            className="tm-name"
-                            value={userid}
-                            disabled={true}
-                        />
-                    </div>
+        <div className="tm-inputs">
+          <div className="tm-input">
+            <i className="fa-solid fa-user" />
+            <input
+              type="text"
+              // placeholder="UserID"
+              className="tm-name"
+              value={userid}
+              disabled={true}
+            />
+          </div>
 
-                    <div className='tm-input'>
-                        <i className="fa-solid fa-award"/>
-                        <input 
-                            type="text" 
-                            // placeholder="AwardID" 
-                            className="tm-name"
-                            value={awardid}
-                            disabled={true}
-                        />
-                    </div>
+          <div className="tm-input">
+            <i className="fa-solid fa-award" />
+            <input
+              type="text"
+              // placeholder="AwardID"
+              className="tm-name"
+              value={awardid}
+              disabled={true}
+            />
+          </div>
 
-                    <div className='tm-input'>
-                        <i className="fa-solid fa-user"/>
-                        <input 
-                            type="text" 
-                            // placeholder="SupplierID" 
-                            className="tm-name"
-                            value={supplierid}
-                            disabled={true}
-                        />
-                    </div>
+          <div className="tm-input">
+            <i className="fa-solid fa-user" />
+            <input
+              type="text"
+              // placeholder="SupplierID"
+              className="tm-name"
+              value={supplierid}
+              disabled={true}
+            />
+          </div>
 
-                    <div className='tm-input'>
-                        <i className="fa-solid fa-usd"/>
-                        <input 
-                            type="text" 
-                            // placeholder="RM..." 
-                            className="tm-name"
-                            value={awardamount}
-                            disabled={true}
-                        />
-                    </div>
+          <div className="tm-input">
+            <i className="fa-solid fa-usd" />
+            <input
+              type="text"
+              // placeholder="RM..."
+              className="tm-name"
+              value={awardamount}
+              disabled={true}
+            />
+          </div>
 
-                    {/* <div className='tm-input'>
+          {/* <div className='tm-input'>
                         <i className="fa-solid fa-file"/>
                         <input 
                             type="text" 
@@ -112,34 +112,34 @@ function SearchAwardID() {
                         />
                     </div> */}
 
-                    <div className='tm-input'>
-                        <i className="fa-solid fa-link"/>
-                        <input 
-                            type="text" 
-                            // placeholder="0x7be3b5f0f43b3ef1f14d26a66997" 
-                            className="tm-name"
-                            value={documenthash}
-                            disabled={true}
-                        />
-                    </div>
+          <div className="tm-input">
+            <i className="fa-solid fa-link" />
+            <input
+              type="text"
+              // placeholder="0x7be3b5f0f43b3ef1f14d26a66997"
+              className="tm-name"
+              value={documenthash}
+              disabled={true}
+            />
+          </div>
 
-                    <div className='tm-input'>
-                        <i className="fa-solid fa-balance-scale"/>
-                        <input 
-                            type="text" 
-                            // placeholder="False" 
-                            className="tm-name"
-                            value={fundstatus}
-                            disabled={true}
-                        />
-                    </div>
-                </div>
-                <button className="tm-btn" onClick={handleBack}>
-                Back
-                </button>
-            </div>
+          <div className="tm-input">
+            <i className="fa-solid fa-balance-scale" />
+            <input
+              type="text"
+              // placeholder="False"
+              className="tm-name"
+              value={fundstatus}
+              disabled={true}
+            />
+          </div>
         </div>
-    );
+        <button className="tm-btn" onClick={handleBack}>
+          Back
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default SearchAwardID;
